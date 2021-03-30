41e2ef45c716390b6639fa479013640830bba686
Davido <d@davidodonovan.com>
2021-03-31 10:23:53 +1100

    create textbook example of wildly inefficient algorithm...

--- mandelbrotFractals/Fractal.js
+++ mandelbrotFractals/Fractal.js
@@ -5,7 +5,7 @@ module.exports = class Fractal {
     this.maxiterations = maxIterations;
     this.palette = [];
     this.counter = 0;
-    this.bigColours = [];
+    this.coloursData = [];
     this.generatePalette();
     this.createBigColoursArray();
   }
@@ -16,10 +16,10 @@ module.exports = class Fractal {
       for(let g=0; g<256; g+=8){
         for(let b=0; b<256; b+=8){
           newArr.push([r, g, b, 255]);
-          this.bigColours.push([r, g, b, 255]);
         };
       };
     };
+    this.coloursData = newArr;
     return newArr;
   };
 
@@ -57,16 +57,16 @@ module.exports = class Fractal {
     };
   };
 
-  fractalizeImage=(imageData)=>{
+  fractalizeImage=(pixelsMatrix)=>{
     // Iterate over the pixels
     for (var y=0; y<this.height; y++) {
       for (var x=0; x<this.width; x++) {
-        this.iterate(x, y, imageData);
+        this.iterate(x, y, pixelsMatrix);
       }
     }
   };
 
-  iterate=(x, y, imageData)=>{
+  iterate=(x, y, pixelsMatrix)=>{
     this.counter++;
     // Iteration variables
     var a = 0;
@@ -85,25 +85,24 @@ module.exports = class Fractal {
       iterations++;
     };
 
-    let color;
-    if (iterations == this.maxIterations) {
-      color = { r:0, g:0, b:0}; // Black
-    } else {
-      color = {r: 255, g:0, b:0 }
-      // var index = Math.floor((iterations / (this.maxIterations-1)) * 255);
-      // color = this.palette[index];
-    }
-    var pixelindex = (y * this.width + x) * 4;
-    imageData.data[pixelindex] = color.r;
-    imageData.data[pixelindex+1] = color.g;
-    imageData.data[pixelindex+2] = color.b;
-    imageData.data[pixelindex+3] = 255;
+    var pixelIndex = (y * this.width + x) * 4;
+    this.pushColours(pixelIndex, pixelsMatrix);
   };
 
-  pushColours=(imageData, pixelIndex)=>{
-    // imageData.data[pixelIndex+1] = this.bigColours[1];
-    // imageData.data[pixelIndex+2] = this.bigColours[2];
-    // imageData.data[pixelIndex+3] = this.bigColours[3];
+  pushColours=( pixelIndex, pixelsMatrix)=>{
+    var colourIndex = pixelIndex/4;
+    let colour;
+    if(colourIndex % 2){
+      colour = this.coloursData.pop()
+    } else {
+      colour = this.coloursData.shift()
+    };
+
+    console.log(this.coloursData.length);
+    pixelsMatrix.data[pixelIndex] = colour[0];
+    pixelsMatrix.data[pixelIndex+1] = colour[1];
+    pixelsMatrix.data[pixelIndex+2] = colour[2];
+    pixelsMatrix.data[pixelIndex+3] = colour[3];
   };
 
 };

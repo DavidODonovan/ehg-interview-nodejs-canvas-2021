54ebd84e6e0657f299bb02de232da22c80313f9d
Davido <d@davidodonovan.com>
2021-03-30 14:39:13 +1100

    use node-canvas lib to create small png

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-console.clear();
-console.log('laksjdf')
+const fs = require('fs');
+const path = require('path');
+const { createCanvas, loadImage } = require('canvas')
+const canvas = createCanvas(200, 200)
+const ctx = canvas.getContext('2d')
+
+// Write "Awesome!"
+ctx.font = '30px Impact'
+ctx.rotate(0.1)
+ctx.fillText('Awesome!', 50, 100)
+
+// Draw line under text
+var text = ctx.measureText('Awesome!')
+ctx.strokeStyle = 'rgba(0,0,0,0.5)'
+ctx.beginPath()
+ctx.lineTo(50, 102)
+ctx.lineTo(50 + text.width, 102)
+ctx.stroke()
+console.log(ctx)
+canvas.createPNGStream().pipe(fs.createWriteStream(path.join(__dirname, 'text.png')))
+// Draw cat with lime helmet
+// loadImage('examples/images/lime-cat.jpg').then((image) => {
+//   ctx.drawImage(image, 50, 0, 70, 70)
+//
+//   console.log('<img src="' + canvas.toDataURL() + '" />')
+// })

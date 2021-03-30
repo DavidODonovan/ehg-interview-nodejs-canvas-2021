4ae083dec152856f7a6379573c7051a240999ec3
Davido <d@davidodonovan.com>
2021-03-31 09:16:14 +1100

    use constructor for global variables

---
+++ mandelbrotFractals/ImageHelper.js
@@ -0,0 +1,49 @@
+module.exports = class ImageHelper {
+  constructor(width, height, maxIterations){
+    this.width = width;
+    this.height = height;
+    this.maxiterations = maxIterations;
+    this.counter = 0;
+  }
+
+  createBigColoursArray=()=>{
+    let newArr = [];
+    for(let r=0; r<256; r+=8){
+      for(let g=0; g<256; g+=8){
+        for(let b=0; b<256; b+=8){
+          newArr.push([r, g, b, 255]);
+        };
+      };
+    };
+    return newArr;
+  };
+
+  pushColoursIntoMatrix=(pixelsMatrix, coloursData)=>{
+    // Loop over all of the pixels
+    for (var x=0; x<this.width; x++) {
+      for (var y=0; y<this.height; y++) {
+        // Get the pixel index
+        var pixelIndex = (y * this.width + x) * 4;
+        var colourIndex = (y * this.width + x);
+        // Set the pixel data
+        pixelsMatrix.data[pixelIndex] = coloursData[colourIndex][0];
+        pixelsMatrix.data[pixelIndex+1] = coloursData[colourIndex][1];
+        pixelsMatrix.data[pixelIndex+2] = coloursData[colourIndex][2];
+        pixelsMatrix.data[pixelIndex+3] = coloursData[colourIndex][3];
+      };
+    };
+  };
+
+  fractalizeImage=(imagew, imageh, imagedata)=>{
+    // Iterate over the pixels
+    for (var y=0; y<imageh; y++) {
+      for (var x=0; x<imagew; x++) {
+        this.iterate(imagew, imageh, x, y, this.maxiterations, imagedata);
+      }
+    }
+  };
+
+  iterate=()=>{
+    console.log(this.counter);
+  };
+};
--- mandelbrotFractals/index.js
+++ mandelbrotFractals/index.js
@@ -1,20 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 const { createCanvas, loadImage } = require('canvas');
-const { createBigColoursArray, pushColoursIntoMatrix, fractalizeImage } = require('./ImageHelpers');
+const ImageHelper = require('./ImageHelper');
 
 const width = 256;
 const height = 128;
-
+const maxIterations = 260;
 const canvas = createCanvas(width, height);
 const ctx = canvas.getContext('2d');
+const imageHelper = new ImageHelper(width, height, maxIterations);
 
-const coloursData = createBigColoursArray();
 const pixelsMatrix = ctx.createImageData(width, height);
+const coloursData = imageHelper.createBigColoursArray();
+imageHelper.pushColoursIntoMatrix(pixelsMatrix, coloursData);
 
-pushColoursIntoMatrix(pixelsMatrix, coloursData, width, height);
-
-fractalizeImage(ctx, width, height, 120, 0);
+// imageHelper.fractalizeImage(width, height, pixelsMatrix);
 
 ctx.putImageData(pixelsMatrix, 0, 0);
 

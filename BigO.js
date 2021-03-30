0cff855f0f31b1ab7d727e23923e5dbc89480847
Davido <d@davidodonovan.com>
2021-03-30 16:57:52 +1100

    update .gitignore, add example shuffle alg

--- BigO.js
+++ BigO.js
@@ -26,6 +26,27 @@ module.exports = {
               pixelsMatrix.data[pixelIndex+3] = coloursData[colourIndex][3];
           };
       };
+  },
+  shuffle: function(array) {
+    let currentIndex = array.length;
+    let temporaryValue;
+    let randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
   }
+
 
 };
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const { createCanvas, loadImage } = require('canvas');
-const { createBigArray, createImage } = require('./BigO');
+const { createBigArray, createImage, shuffle } = require('./BigO');
 
 const width = 256;
 const height = 128;
@@ -13,6 +13,14 @@ const coloursData = createBigArray();
 const pixelsMatrix = ctx.createImageData(width, height);
 
 createImage(pixelsMatrix, coloursData, width, height);
+console.log(pixelsMatrix.data.length);
 ctx.putImageData(pixelsMatrix, 0, 0);
 
+
 canvas.createPNGStream().pipe(fs.createWriteStream(path.join(__dirname, 'colours.png')))
+
+
+// Used like so
+var arr = [2, 11, 37, 42];
+shuffle(arr);
+console.log(arr);

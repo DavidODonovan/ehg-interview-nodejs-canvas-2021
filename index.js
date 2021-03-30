3422dfae566bcfb5a7e31bf9e442d3b9fc0bb3b6
Davido <d@davidodonovan.com>
2021-03-30 15:23:18 +1100

    successfully create pixel-by-pixel image using image data object

--- index.js
+++ index.js
@@ -15,6 +15,32 @@ const ctx = canvas.getContext('2d');
 let myData = ctx.createImageData(width, height);
 console.log(myData.data)//<----each pixel has 4 values for rgba
 
+function createImage(offset) {
+    // Loop over all of the pixels
+    for (var x=0; x<width; x++) {
+        for (var y=0; y<height; y++) {
+            // Get the pixel index
+            var pixelindex = (y * width + x) * 4;
+
+            // Generate a xor pattern with some random noise
+            var red = ((x+offset) % 256) ^ ((y+offset) % 256);
+            var green = ((2*x+offset) % 256) ^ ((2*y+offset) % 256);
+            var blue = 50 + Math.floor(Math.random()*100);
+
+            // Rotate the colors
+            blue = (blue + offset) % 256;
+
+            // Set the pixel data
+            myData.data[pixelindex] = red;     // Red
+            myData.data[pixelindex+1] = green; // Green
+            myData.data[pixelindex+2] = blue;  // Blue
+            myData.data[pixelindex+3] = 255;   // Alpha
+        }
+    }
+}
+createImage(Math.floor(0 / 10));
+ctx.putImageData(myData, 0, 0);
+
 
 // pipe stream to file on local hard drive
 canvas.createPNGStream().pipe(fs.createWriteStream(path.join(__dirname, 'text.png')))

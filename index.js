22126c9fc57bc24eabcb4608ac70151ea1506741
Davido <d@davidodonovan.com>
2021-03-30 14:48:51 +1100

    import BigO and create data

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const path = require('path');
 const { createCanvas, loadImage } = require('canvas')
 const canvas = createCanvas(200, 200)
 const ctx = canvas.getContext('2d')
-
-
+const BigO = require('./BigO');
+const data = BigO.createBigArray();
+console.log(data)
 
 // pipe stream to file on local hard drive
 canvas.createPNGStream().pipe(fs.createWriteStream(path.join(__dirname, 'text.png')))

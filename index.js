029ce4e7998fb3ce06bde720f227cb63891ac269
Davido <d@davidodonovan.com>
2021-03-30 15:00:24 +1100

    copy over challenge requirements

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const { createCanvas, loadImage } = require('canvas')
 
 const data = BigO.createBigArray();
 
-const width = 500;
-const height = 500;
+const width = 128;
+const height = 56;
 const canvas = createCanvas(width, height);
 const ctx = canvas.getContext('2d');
 
 let myData = ctx.createImageData(width, height);
-console.log(myData)
+console.log(myData.data.length)
 
 
 // pipe stream to file on local hard drive

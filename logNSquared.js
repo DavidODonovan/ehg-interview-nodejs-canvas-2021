04b2391d5ed8647d3408813d5850d83192599c92
Davido <d@davidodonovan.com>
2021-03-30 14:46:17 +1100

    return big array from nested for loop

--- logNSquared.js
+++ logNSquared.js
@@ -16,16 +16,13 @@ class Thing {
   nestedForLoop=()=>{
     let newArr = [];
     for(let i=0; i<32; i++){
-      console.log("i:",i);
       for(let j =0; j<32; j++){
-        console.log("j:",j);
         for(let k =0; k<32; k++){
-          console.log(`rgb(${i}, ${j}, ${k})`)
           newArr.push(`rgb(${i}, ${j}, ${k})`)
         }
       }
     };
-   console.log(newArr.length)
+    return newArr;
   };
 
 };

444cff6256a6b4fc4a023655b62f9b2d355346d0
Davido <d@davidodonovan.com>
2021-03-30 16:25:29 +1100

    it's ugly/hackey but it works thus far

--- BigO.js
+++ BigO.js
@@ -2,12 +2,12 @@ class BigO {
 
   createBigArray=()=>{
     let newArr = [];
-    for(let i=0; i<32; i++){
-      for(let j =0; j<32; j++){
-        for(let k =0; k<32; k++){
+    for(let i=0; i<256; i+=8){
+      for(let j=0; j<256; j+=8){
+        for(let k=0; k<256; k+=8){
           newArr.push([i, j, k, 255]);
-        }
-      }
+        };
+      };
     };
     return newArr;
   };
--- index.js
+++ index.js
@@ -9,9 +9,11 @@ const canvas = createCanvas(width, height);
 const ctx = canvas.getContext('2d');
 
 const coloursData = BigO.createBigArray();
-console.log(coloursData.length*4);
+const pixelsMatrix = ctx.createImageData(width, height);
 
-let pixelsMatrix = ctx.createImageData(width, height);
+
+console.log(coloursData[1]);
+console.log(coloursData.length);
 console.log(pixelsMatrix.data.length);
 
 function createImage() {
@@ -20,18 +22,17 @@ function createImage() {
         for (var y=0; y<height; y++) {
             // Get the pixel index
             var pixelIndex = (y * width + x) * 4;
-            // console.log(coloursData[pixelIndex]);
-
+            var colourIndex = (y * width + x);
             // Set the pixel data
-            // pixelsMatrix.data[pixelIndex] = coloursData[pixelIndex][0];
-            // pixelsMatrix.data[pixelIndex+1] = coloursData[pixelIndex][1];
-            // pixelsMatrix.data[pixelIndex+2] = coloursData[pixelIndex][2];
-            // pixelsMatrix.data[pixelIndex+3] = coloursData[pixelIndex][3];
+            pixelsMatrix.data[pixelIndex] = coloursData[colourIndex][0];
+            pixelsMatrix.data[pixelIndex+1] = coloursData[colourIndex][1];
+            pixelsMatrix.data[pixelIndex+2] = coloursData[colourIndex][2];
+            pixelsMatrix.data[pixelIndex+3] = coloursData[colourIndex][3];
         }
     }
 }
 createImage();
-// ctx.putImageData(pixelsMatrix, 0, 0);
+ctx.putImageData(pixelsMatrix, 0, 0);
 
 
 // pipe stream to file on local hard drive

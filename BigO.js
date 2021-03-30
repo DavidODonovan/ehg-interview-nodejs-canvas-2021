c445477cb9e93473a44eba1eb05423f3cb450723
Davido <d@davidodonovan.com>
2021-03-30 16:43:31 +1100

    break functions out into separate file

--- BigO.js
+++ BigO.js
@@ -1,17 +1,31 @@
-class BigO {
+module.exports = {
 
-  createBigArray=()=>{
+  createBigArray: function(){
     let newArr = [];
-    for(let i=0; i<256; i+=8){
-      for(let j=0; j<256; j+=8){
-        for(let k=0; k<256; k+=8){
-          newArr.push([i, j, k, 255]);
+    for(let r=0; r<256; r+=8){
+      for(let g=0; g<256; g+=8){
+        for(let b=0; b<256; b+=8){
+          newArr.push([r, g, b, 255]);
         };
       };
     };
     return newArr;
-  };
+  },
+
+  createImage: function(pixelsMatrix, coloursData, width, height) {
+      // Loop over all of the pixels
+      for (var x=0; x<width; x++) {
+          for (var y=0; y<height; y++) {
+              // Get the pixel index
+              var pixelIndex = (y * width + x) * 4;
+              var colourIndex = (y * width + x);
+              // Set the pixel data
+              pixelsMatrix.data[pixelIndex] = coloursData[colourIndex][0];
+              pixelsMatrix.data[pixelIndex+1] = coloursData[colourIndex][1];
+              pixelsMatrix.data[pixelIndex+2] = coloursData[colourIndex][2];
+              pixelsMatrix.data[pixelIndex+3] = coloursData[colourIndex][3];
+          };
+      };
+  }
 
 };
-
-module.exports = new BigO();
--- index.js
+++ index.js
@@ -1,39 +1,20 @@
-const BigO = require('./BigO');
 const fs = require('fs');
 const path = require('path');
-const { createCanvas, loadImage } = require('canvas')
+const { createCanvas, loadImage } = require('canvas');
+const { createBigArray, createImage } = require('./BigO');
 
 const width = 256;
 const height = 128;
+
 const canvas = createCanvas(width, height);
 const ctx = canvas.getContext('2d');
 
-const coloursData = BigO.createBigArray();
+const coloursData = createBigArray();
 const pixelsMatrix = ctx.createImageData(width, height);
 
 
-console.log(coloursData[1]);
-console.log(coloursData.length);
-console.log(pixelsMatrix.data.length);
+createImage(pixelsMatrix, coloursData, width, height);
 
-function createImage() {
-    // Loop over all of the pixels
-    for (var x=0; x<width; x++) {
-        for (var y=0; y<height; y++) {
-            // Get the pixel index
-            var pixelIndex = (y * width + x) * 4;
-            var colourIndex = (y * width + x);
-            // Set the pixel data
-            pixelsMatrix.data[pixelIndex] = coloursData[colourIndex][0];
-            pixelsMatrix.data[pixelIndex+1] = coloursData[colourIndex][1];
-            pixelsMatrix.data[pixelIndex+2] = coloursData[colourIndex][2];
-            pixelsMatrix.data[pixelIndex+3] = coloursData[colourIndex][3];
-        }
-    }
-}
-createImage();
 ctx.putImageData(pixelsMatrix, 0, 0);
-
-
 // pipe stream to file on local hard drive
 canvas.createPNGStream().pipe(fs.createWriteStream(path.join(__dirname, 'colours.png')))

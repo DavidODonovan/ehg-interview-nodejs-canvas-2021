8125b1816bcec54ce4655629312a834e53a696e6
Davido <d@davidodonovan.com>
2021-03-30 14:58:03 +1100

    grab image data

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+const BigO = require('./BigO');
 const fs = require('fs');
 const path = require('path');
 const { createCanvas, loadImage } = require('canvas')
-const canvas = createCanvas(200, 200)
-const ctx = canvas.getContext('2d')
-const BigO = require('./BigO');
+
 const data = BigO.createBigArray();
-console.log(data)
+
+const width = 500;
+const height = 500;
+const canvas = createCanvas(width, height);
+const ctx = canvas.getContext('2d');
+
+let myData = ctx.createImageData(width, height);
+console.log(myData)
+
 
 // pipe stream to file on local hard drive
 canvas.createPNGStream().pipe(fs.createWriteStream(path.join(__dirname, 'text.png')))

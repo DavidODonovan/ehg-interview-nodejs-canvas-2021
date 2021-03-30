3d0fd89fd377a9e9d7fda40eaa08639b155f2dd0
Davido <d@davidodonovan.com>
2021-03-31 10:39:52 +1100

    delete old code, keep failed Fractal class

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const fs = require('fs');
 const path = require('path');
 const { createCanvas, loadImage } = require('canvas');
-const { createBigArray, createImage } = require('./BigO');
-const fractals = require('./mandelbrotFractals');
+const Fractal = require('./Fractal');
 
-const width = 256;
-const height = 128;
+const width = 4096;
+const height = 8;
+const maxIterations = 250;
+
 
 const canvas = createCanvas(width, height);
 const ctx = canvas.getContext('2d');
-
-const coloursData = createBigArray();
 const pixelsMatrix = ctx.createImageData(width, height);
 
-createImage(pixelsMatrix, coloursData, width, height);
+const fractal = new Fractal(width, height, maxIterations);
+const coloursData = fractal.createBigColoursArray();
+
+fractal.fractalizeImage(pixelsMatrix);
+
 ctx.putImageData(pixelsMatrix, 0, 0);
 
-canvas.createPNGStream().pipe(fs.createWriteStream(path.join(__dirname, 'colours.png')))
+canvas.createPNGStream().pipe(fs.createWriteStream(path.join(__dirname, 'mandelbrotFractals.png')))

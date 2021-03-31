f3a18119c04bf8f9b31694b39ccf7ff3cfe0afd4
Davido <d@davidodonovan.com>
2021-03-31 19:20:18 +1100

    conditional render: dims.width && <...>

--- frontend/src/components/ImageContainer/index.js
+++ frontend/src/components/ImageContainer/index.js
@@ -2,17 +2,15 @@ import { withContext } from 'contexts/ReactDims';
 import { StyledImage, Container } from './style';
 import allTheColours from './allTheColours.png';
 
-const ImageContainer=(props)=>{
+const ImageContainer=({ dims })=>{
 
-  console.log(props.dims);
-  // TODO: 1) change AppWrapper to grid and move Container to center 
-  //  TODO: 2) conditional render of img when props.dims is present, useEffect.
+  // TODO: 1) change AppWrapper to grid and move Container to center
   // TODO: 3) pass dims down to style and render translations using dims and image size.
 
   return (
     <Container>
       <StyledImage>
-        <img src={allTheColours}/>
+        {dims.width && (<img src={allTheColours}/>)}
       </StyledImage>
     </Container>
   );

7872b13571d75c15ba85f996441256e5e407ab08
Davido <d@davidodonovan.com>
2021-03-31 17:43:12 +1100

    add keyframes and animate a little

--- frontend/src/style.js
+++ frontend/src/style.js
@@ -1,13 +1,29 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 export const Container=styled.div`
   border: 1px solid red;
   width: 100%;
   height: 100vh;
+  overflow-x: hidden;
+`;
+
+const imageAnim = keyframes`
+	0% {
+		transform: translateX(0px);
+    trans
+	}
+	50% {
+		transform: translateX(-1024px);
+	}
+	100% {
+		transform: translateX(-2048px);
+	}
 `;
 
 export const StyledImage=styled.div`
   img {
+    animation: ${imageAnim} 4s linear;
+    transform: translateX(-100px);
     image-rendering: optimizeSpeed;             /*                     */
     image-rendering: -moz-crisp-edges;          /* Firefox             */
     image-rendering: -o-crisp-edges;            /* Opera               */
@@ -19,3 +35,25 @@ export const StyledImage=styled.div`
 `;
 
 // css source: // https://superuser.com/questions/530317/how-to-prevent-chrome-from-blurring-small-images-when-zoomed-in
+
+//
+//
+// export const CircularBells=styled(Circles).attrs((props)=>({
+// }))`
+//   & circle {
+//     cursor: pointer;
+//     fill: red;
+// 		/* discovered a new filter!*/
+// 		filter: drop-shadow(0 0.2rem 0.25rem rgba(0, 0, 0, 1));
+//
+//     /* use weird $ syntax on props */
+//     /* also wrap return value in css helper */
+//     ${({$clicked})=>{
+//       if($clicked){
+//         return css`
+// 					animation: ${buttonAnim} .4s linear;
+// 					`;
+//       }
+//     }};
+//   }
+// `;

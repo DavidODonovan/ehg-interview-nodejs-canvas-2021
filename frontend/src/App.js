65d7bdc516d40f802316b3e41e9c05e485a413c9
Davido <d@davidodonovan.com>
2021-03-31 11:13:32 +1100

    doesnt look right: looks like webpack is compressing the image...

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import colours from './allTheColours.png';
 const App=()=>{
   return (
     <div className="App">
-      <img src={colours} style={{height: "400px"}}/>
+      <img src={colours} style={{height: "4000px"}}/>
     </div>
   );
 };
